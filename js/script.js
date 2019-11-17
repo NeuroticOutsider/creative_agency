d738731e83dd5c265e573ea65e7f627767326152
NeuroticOutsider <klimberg-nikita@yandex.ru>
2019-11-18 01:23:09 +0300

    Header done + adaptive

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 $(function() {
+  $('.navbar-toggler').click(function(){
+    var menuBtn = $('.navbar-toggler .fa'),
+        body = $('body');
 
+    $(this).toggleClass('clicked');
+
+    if ($(this).hasClass('clicked')) {
+      menuBtn.removeClass('fa-bars');
+      menuBtn.addClass('fa-times');
+      body.addClass('bg-dark');
+    } else {
+      menuBtn.removeClass('fa-times');
+      menuBtn.addClass('fa-bars');
+      body.removeClass('bg-dark');
+    }
+  });
 });
-
